d7f6b32a7b0a3a20b395e48cfce5be36369d752c
NSF2023 <NSF2023@csedept.uhhilo>
2023-06-20 16:21:28 -1000

    troubleshooting next_task to add new tas

--- label_studio/data_manager/actions/next_task.py
+++ label_studio/data_manager/actions/next_task.py
@@ -1,6 +1,8 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
 import logging
+import subprocess
+import os
 
 from rest_framework.exceptions import NotFound
 from data_manager.functions import filters_ordering_selected_items_exist
@@ -10,6 +12,15 @@ from tasks.serializers import NextTaskSerializer
 
 
 logger = logging.getLogger(__name__)
+
+
+def create_new_task():
+    print("BEGIN CREATE NEW PROJECT")
+
+    python_file = 'C:\\Users\\nsf2023\\repos\\landcoveranalysis\\getJsonWithId.py'
+    # Call the function in Python file as a subprocess
+    os.system(f"C:\\Users\\nsf2023\\.conda\\envs\\researchEnv\\python.exe {python_file} C:\\Users\\nsf2023\\repos\\completedPath\\")
+    # create_new_task_in_file {str(project.id)}")
 
 
 def next_task(project, queryset, **kwargs):
@@ -23,13 +34,17 @@ def next_task(project, queryset, **kwargs):
     request = kwargs['request']
     dm_queue = filters_ordering_selected_items_exist(request.data)
     next_task, queue_info = get_next_task(request.user, queryset, project, dm_queue)
-
+    print("QUERYSET", queryset)
     if next_task is None:
-        raise NotFound(
-            f'There are still some tasks to complete for the user={request.user}, '
-            f'but they seem to be locked by another user.')
+        # create task if none is found
+        create_new_task()
 
-    # serialize task
+        next_task, queue_info = get_next_task(request.user, queryset, project, dm_queue)
+        # raise NotFound(
+        #      f' There are still some tasks to complete for the user={request.user}, '
+        #      f'but they seem to be locked by another user.')
+
+        # serialize task
     context = {'request': request, 'project': project, 'resolve_uri': True, 'annotations': False}
     serializer = NextTaskSerializer(next_task, context=context)
     response = serializer.data
